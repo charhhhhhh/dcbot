165e2b0ac33c3a591fe047d2ddcdd23a1dd9ae65
charhhhhhh <charliewang1201@gmail.com>
2023-02-26 22:19:07 +0800

    ver 2.4

---
+++ store/app.js
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia"
+export const usAppStore = defineStore('app', {
+    state: () => ({}),//狀態
+    getters: {},
+    actions: {},
+})
+//記得逗號!!!!!!!!!!!!!!!!!!!!!!!!!!!!
